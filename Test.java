1a00861fd5a97232ba8f835f9923a1bd888c7dbf
jeongsoo.Park <dduruwjdtn@naver.com>
2020-05-06 13:08:13 +0900

    병 합

--- Test.java
+++ Test.java
@@ -1,11 +1,7 @@
 class Test{
 	public static void main(String [] args){
-<<<<<<< HEAD
 		System.out.println("Hello");
 		System.out.println("Git");
-=======
-		System.out.println("Giti");
->>>>>>> branch2
 		System.out.println("Hi Git");
 		System.out.println("branch1");
 		System.out.println("branch2");
